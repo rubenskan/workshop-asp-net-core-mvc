f2977367168536f4028731107392b8d9dbd7d32b
rubenskan@gmail.com <jfmpp!@0171>
2019-12-11 11:01:57 -0300

    Adiconando view index

---
+++ SalesWebMvc/Migrations/20191211133049_AdiconadoCampoSeller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class AdiconadoCampoSeller : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Seller");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Seller",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Seller");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Seller",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
